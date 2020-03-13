21e9c1f60caa3bb216d927645d126f97b5070dee
Shawn DelPercio <sdelperc@gmail.com>
2020-03-13 11:45:51 -0400

    added middleware to validate project routes

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const projectRouter = require('../projects/projects-router');
+const actionRouter = require('../actions/actions-router');
 
 const server = express();
+server.use(logger);
 server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 server.get('/', (req, res) => {
 	res.status(200).send('API Sprint!!');
 });
+
+//custom middleware
+function logger(req, res, next) {
+	const method = req.method;
+	const path = req.originalUrl;
+	console.log(`Request logged: ${method} to ${path}`);
+	next();
+}
 
 module.exports = server;
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,5 +1,119 @@
 const express = require('express');
+const projectDb = require('../data/helpers/projectModel');
 
 const router = express.Router();
+
+// GET all projects
+router.get('/', (req, res) => {
+	// to do
+});
+
+// GET a single project
+router.get('/:id', (req, res) => {
+	// to do
+});
+
+// GET a project's actions
+router.get('/:id/actions', (req, res) => {
+	// to do
+});
+
+// POST a new action to a project
+router.post('/:id/actions', (req, res) => {
+	// to do
+});
+
+// POST a project
+router.post('/', (req, res) => {
+	// to do
+});
+
+// UPDATE a project
+router.put('/:id', (req, res) => {
+	// to do
+});
+
+// DELETE a project
+router.delete('/:id', (req, res) => {
+	// to do
+});
+
+// Project Middleware
+function validateProjectId(req, res, next) {
+	const id = req.params.id;
+
+	if (!id) {
+		res.status(400).json({ error: 'Must include project ID in URL.' });
+	} else {
+		projectDb
+			.get(id)
+			.then(project => {
+				if (project === undefined) {
+					res.status(400).json({ error: 'Project does not exist.' });
+				} else {
+					next();
+				}
+			})
+			.catch(err => {
+				res.status(500).json({
+					error: 'There was an issue with retrieving the project ID.',
+					err
+				});
+			});
+	}
+}
+
+function validateProject(req, res, next) {
+	const body = req.body;
+	const name = req.body.name;
+	const desc = req.body.description;
+
+	if (Object.keys(body).length === 0) {
+		res.status(400).json({ error: 'Missing project data.' });
+	} else if (!name) {
+		res.status(400).json({ error: 'Missing required name field.' });
+	} else if (!desc) {
+		res.status(400).json({ error: 'Missing required description field.' });
+	} else {
+		next();
+	}
+}
+function validateAction(req, res, next) {
+	const body = req.body;
+	const desc = req.body.description;
+	const notes = req.body.notes;
+	const projectId = req.body.project_id;
+
+	if (Object.keys(body).length === 0) {
+		res.status(400).json({ error: 'Missing action data.' });
+	} else if (!notes) {
+		res.status(400).json({ error: 'Missing required name field.' });
+	} else if (!desc || desc.length > 128) {
+		res
+			.status(400)
+			.json({
+				error:
+					'Missing required description field or description longer than 128 characters.'
+			});
+	} else if (!projectId) {
+		res.status(400).json({ error: 'Must include project ID in request.' });
+	} else {
+		projectDb
+			.get(projectId)
+			.then(project => {
+				if (project === undefined) {
+					res.status(400).json({ error: 'Project does not exist.' });
+				} else {
+					next();
+				}
+			})
+			.catch(err => {
+				res.status(500).json({
+					error: 'There was an issue with retrieving the project ID.',
+					err
+				});
+			});
+	}
+}
 
 module.exports = router;
